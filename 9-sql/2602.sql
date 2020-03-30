f48bc5f9ffb6470508373efcf5db316efc898144
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2020-03-30 08:28:53 -0300

    Added solutions for 2602 & 2603 problems.

---
+++ 9-sql/2602.sql
@@ -0,0 +1,5 @@
+-- 2602 - Simple Select
+
+SELECT name
+FROM customers
+WHERE state LIKE 'RS';
---
+++ 9-sql/2603.sql
@@ -0,0 +1,5 @@
+-- 2603 - Customer Address
+
+SELECT name, street
+FROM customers
+WHERE city LIKE 'Porto Alegre';
