11384ff63f51f702d7824e466018e98b887674ff
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2020-03-31 11:24:50 -0300

    Added solutions for problem 1005.

---
+++ 1-beginner/1005.js
@@ -0,0 +1,10 @@
+// 1005 - Average 1
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const gradeA = parseFloat(lines.shift()) * 3.5; // Multiply by weight
+const gradeB = parseFloat(lines.shift()) * 7.5; // Multiply by weight
+const media = ((gradeA+gradeB)/11); // 11 = 3.5 + 7.5
+
+console.log(`MEDIA = ${media.toFixed(5)}`);
