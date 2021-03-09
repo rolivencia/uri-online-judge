d06feb3ffbf7afe194f732f906e5c8a03cb2fd1b
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-03-09 00:31:48 -0300

    Added solution to problem 1006

---
+++ 1-beginner/1006.js
@@ -0,0 +1,12 @@
+// 1006 - Average 21
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n').map(line => parseFloat(line).toFixed(1));
+
+//const lines = [10, 10, 5];
+
+const a = lines[0];
+const b = lines[1];
+const c = lines[2];
+
+console.log('MEDIA = ' + ((a*2+b*3+c*5)/10).toFixed(1));
