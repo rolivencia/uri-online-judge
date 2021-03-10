6a0a89cfa4ac5cd23241f82864aee47ace7aff40
Olivencia.Ramiro <ramiro.olivencia@kbcat.com>
2021-03-10 20:22:04 -0300

    Added solution to problem 1007

---
+++ 1-beginner/1007.js
@@ -0,0 +1,11 @@
+// 1007 - Difference
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const numbers = input.split('\n').map(line => parseInt(line));
+
+const a = numbers[0];
+const b = numbers[1];
+const c = numbers[2];
+const d = numbers[3];
+
+console.log('DIFERENCA = ' + (a*b - c*d));
