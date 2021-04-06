e236c19a69dc5ccd54af7fb0bd60b23fec14bbf2
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-06 09:11:35 -0300

    Added solution to problem 1010

---
+++ 1-beginner/1010.js
@@ -0,0 +1,17 @@
+// 1010 - Simple Calculate
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+//
+
+const product1Raw = lines.shift().split(' ');
+const product2Raw = lines.shift().split(' ');
+
+const product1 = {code: parseInt(product1Raw[0]), units: parseInt(product1Raw[1]), price: parseFloat(product1Raw[2])};
+const product2 = {code: parseInt(product2Raw[0]), units: parseInt(product2Raw[1]), price: parseFloat(product2Raw[2])};
+
+const product1Amount = product1.units * product1.price;
+const product2Amount = product2.units * product2.price;
+
+console.log('VALOR A PAGAR: R$ ' + (product1Amount + product2Amount).toFixed(2));
