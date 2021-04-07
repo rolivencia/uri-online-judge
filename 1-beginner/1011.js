ff193ef71ca575fc0ac8cf34f9a68eb53f2f15f8
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-07 00:49:10 -0300

    Added solution to problem 1011

---
+++ 1-beginner/1011.js
@@ -0,0 +1,10 @@
+//1011 - Sphere
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const pi = 3.14159;
+const radius = parseFloat(lines.shift());
+const volume = (4.0/3) * pi * Math.pow(radius,3);
+
+console.log('VOLUME = ' + volume.toFixed(3));
