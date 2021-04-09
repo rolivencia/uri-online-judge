2c0680202c5bca5aa83fbec946fbe000bad23ade
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-09 20:34:57 -0300

    Added solution to problem 1013

---
+++ 1-beginner/1013.js
@@ -0,0 +1,11 @@
+//1013 - The Greatest
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const values = lines.shift().split(' ').map(value => parseInt(value));
+const greater = (a,b) => (a+b+Math.abs(a-b))/2;
+
+const result = greater(greater(values[0], values[1]), values[2]);
+
+console.log(result + " eh o maior");
