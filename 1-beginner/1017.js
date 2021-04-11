ad9cb1759c26a8fc5f9da689a94937317e5e14f3
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-11 12:36:14 -0300

    Added solution to problem 1017

---
+++ 1-beginner/1017.js
@@ -0,0 +1,14 @@
+//1017 - Fuel Spent
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n').map(value => parseInt(value));
+
+const performance = 12.0; // in Km/L
+const tripDuration = lines[0]; // in hours
+const averageSpeed = lines[1]; // in Km/h
+
+const distance = tripDuration * averageSpeed; // in Km - hours gets cancelled out
+const fuelRequired = distance / performance; // in L - Km gets cancelled out
+
+console.log(fuelRequired.toFixed(3));
+
