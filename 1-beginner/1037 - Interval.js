dcf122cde4449af7939ebc1a7641225b28ab266d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-14 19:35:54 -0300

    Added solution to problem 1037.

---
+++ 1-beginner/1037 - Interval.js
@@ -0,0 +1,24 @@
+// 1037 - Interval
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const value = parseFloat(input);
+
+if(value < 0 || value > 100){
+    console.log('Fora de intervalo');
+}
+else{
+    switch(true){
+        case (value <= 25):
+            console.log('Intervalo [0,25]');
+            break;
+        case (value <= 50):
+            console.log('Intervalo (25,50]');
+            break;
+        case (value <= 75):
+            console.log('Intervalo (50,75]');
+            break;
+        case (value <= 100):
+            console.log('Intervalo (75,100]');
+            break;
+    }
+}
