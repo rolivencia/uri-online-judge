9e39c484ccce57f7fd0640f6d404661be9860355
Olivencia.Ramiro <ramiro.olivencia@kbcat.com>
2021-04-16 23:41:47 -0300

    Added solution to problem 1038.

---
+++ 1-beginner/1038 - Snack.js
@@ -0,0 +1,16 @@
+// 1038 - Snack
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const [idProduct, quantity] = input.split(" ").map(value => parseInt(value));
+
+const table = [
+    {id: 1, specification: "Cachorro Quente", price: 4 },
+    {id: 2, specification: "X-Salada", price: 4.50 },
+    {id: 3, specification: "X-Bacon", price: 5 },
+    {id: 4, specification: "Torrada simples", price: 2 },
+    {id: 5, specification: "Refrigerante", price: 1.50 }
+];
+
+const pickedProduct = table.filter(product => product.id === idProduct).pop();
+
+console.log(`Total: R$ ${(quantity * pickedProduct.price).toFixed(2)}`);
