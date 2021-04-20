7054374f3c217424d71196d80f22b9357210b79d
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-20 01:28:59 -0300

    Added solution to problem 1043.

---
+++ 1-beginner/1043 - Triangle.js
@@ -0,0 +1,23 @@
+// 1043 - Triangle
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+const compare = (a, b) =>  {
+    if (a<b) {
+        return -1;
+    }
+    if (a>b) {
+        return 1;
+    }
+    return 0;
+}
+
+const [a, b, c] = input.split(" ").map(value => parseFloat(value));
+const [sortedA, sortedB, sortedC] = input.split(" ").map(value => parseFloat(value)).sort(compare);
+
+if(sortedC < sortedA + sortedB){
+    console.log(`Perimetro = ${(a + b + c).toFixed(1)}`);
+}
+else{
+    console.log(`Area = ${(((a + b)/2) * c).toFixed(1)}`);
+}
