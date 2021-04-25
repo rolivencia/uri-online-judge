f0dfc6b6957552d3cecc6b2271620e41012b1046
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-04-25 19:27:47 -0300

    Added solution to problem 1048

---
+++ 1-beginner/1048 - Salary Increase.js
@@ -0,0 +1,19 @@
+// 1048 - Salary Increase
+// URL - https://www.urionlinejudge.com.br/judge/en/problems/view/1048
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const previousSalary = parseFloat(input);
+
+const scales = [
+    { min: 0, max: 400.00, rate: 0.15 },
+    { min: 400.01, max: 800.00, rate: 0.12 },
+    { min: 800.01, max: 1200.00, rate: 0.10 },
+    { min: 1200.01, max: 2000.00, rate: 0.07 },
+    { min: 2000.01, max: Infinity, rate: 0.04 },
+];
+
+const scale = scales.filter(scale => scale.min <= previousSalary && scale.max >= previousSalary).pop();
+
+console.log(`Novo salario: ${ (previousSalary * (1 + scale.rate)).toFixed(2) }`);
+console.log(`Reajuste ganho: ${ (previousSalary * scale.rate).toFixed(2) } `);
+console.log(`Em percentual: ${ (scale.rate * 100).toFixed(0) } %`);
