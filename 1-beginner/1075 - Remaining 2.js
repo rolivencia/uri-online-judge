8c21afd0f0c18841353ee4444af34560358db914
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-05-22 13:53:48 -0300

    Added solution to problem 1075

---
+++ 1-beginner/1075 - Remaining 2.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+const divider = parseInt(input);
+
+for (let i = 1; i <= 10000; i++) {
+  if (i % divider === 2) {
+    console.log(i);
+  }
+}
+
---
+++ template.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
