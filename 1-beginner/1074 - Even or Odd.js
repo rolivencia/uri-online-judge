c9404e1eece7c37c3a65628e8c6fcbea6062f7ea
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-05-22 13:47:35 -0300

    Fixed solution to problem 1074

--- 1-beginner/1074 - Even or Odd.js
+++ 1-beginner/1074 - Even or Odd.js
@@ -2,27 +2,27 @@ const input = require("fs").readFileSync("/dev/stdin", "utf8");
 
 const values = input
   .split("\n")
-  .map((value) => parseInt(value))
-  .slice(1); // I remove the first value
+  .slice(1) // I remove the first value
+  .map((value) => parseInt(value));
 
 for (let value of values) {
-  let output = "";
   if (value === 0) {
     console.log("NULL");
-    continue;
-  }
-
-  if (value % 2 === 0) {
-    output = output.concat("EVEN ");
-  } else if (value % 2 !== 0) {
-    output = output.concat("ODD ");
   }
 
   if (value > 0) {
-    output = output.concat("POSITIVE");
-  } else if (value < 0) {
-    output = output.concat("NEGATIVE");
+    if (value % 2 === 0) {
+      console.log("EVEN POSITIVE");
+    } else if (value % 2 !== 0) {
+      console.log("ODD POSITIVE");
+    }
+  }
+  if (value < 0) {
+    if (value % 2 === 0) {
+      console.log("EVEN NEGATIVE");
+    } else if (value % 2 !== 0) {
+      console.log("ODD NEGATIVE");
+    }
   }
 
-  console.log(output);
 }
