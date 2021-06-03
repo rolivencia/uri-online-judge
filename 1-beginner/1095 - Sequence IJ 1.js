cbdcec6f3788f12c98b3fb9fadde450737953f91
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-06-02 23:50:21 -0300

    Added solution to problem 1095

---
+++ 1-beginner/1095 - Sequence IJ 1.js
@@ -0,0 +1,11 @@
+// 1095 - Sequence IJ 1
+// URL - https://www.urionlinejudge.com.br/judge/en/problems/view/1095
+
+let i = 1;
+let j = 60;
+
+while(j>=0){
+    console.log(`I=${i} J=${j}`);
+    j-=5;
+    i+=3;
+}
