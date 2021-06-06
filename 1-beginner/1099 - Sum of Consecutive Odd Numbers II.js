4ad920aafa65954c4fe1c9b417727492c2b68db5
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-06-06 20:20:03 -0300

    Added solution to problem 1099

---
+++ 1-beginner/1099 - Sum of Consecutive Odd Numbers II.js
@@ -0,0 +1,17 @@
+// 1099 - Sum of Consecutive Odd Numbers II
+// URL - https://www.urionlinejudge.com.br/judge/en/problems/view/1099
+
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+const lines = input.split("\n").slice(1);
+
+for (let line of lines) {
+  const [start, end] = line.split(" ").map((value) => parseInt(value)).sort();
+
+  let sum = 0;
+  for (let i = start + 1; i < end; i++) {
+    if (i % 2 !== 0) {
+      sum += i;
+    }
+  }
+  console.log(sum);
+}
