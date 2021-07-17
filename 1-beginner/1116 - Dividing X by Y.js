5df0c88491fb1cdb3f475dd3ec94387335799c06
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-07-17 06:33:31 -0300

    Added functional approach for 1116

--- 1-beginner/1116 - Dividing X by Y.js
+++ 1-beginner/1116 - Dividing X by Y.js
@@ -1,9 +1,9 @@
 // 1116 - Dividing X by Y
 // URL - https://www.urionlinejudge.com.br/judge/en/problems/view/1116
 
-// const input = "4\n" + "2 3\n" + "-3 -5\n" + "-4 3\n" + "2 -4";
+const input = "4\n" + "2 3\n" + "-3 -5\n" + "-4 3\n" + "2 -4";
 
-const input = require("fs").readFileSync("/dev/stdin", "utf8");
+// const input = require("fs").readFileSync("/dev/stdin", "utf8");
 const values = input
   .split("\n")
   .filter((value) => !!value)
@@ -11,13 +11,20 @@ const values = input
   .map((value) => value.split(" "))
   .map((pair) => ({ x: parseInt(pair.shift()), y: parseInt(pair.shift()) }));
 
+// console.log(values);
+
 // Imperative printing
 for (const pair of values) {
-  if (pair.y === 0) {
-    console.log("divisao impossivel");
-  } else {
-    console.log((pair.x / pair.y).toFixed(1));
-  }
+  pair.y === 0
+    ? console.log("divisao impossivel")
+    : console.log((pair.x / pair.y).toFixed(1));
 }
 
-// console.log(values);
+// Functional printing
+values.map((pair) =>
+  pair.y === 0
+    ? console.log("divisao impossivel")
+    : console.log((pair.x / pair.y).toFixed(1))
+);
+
+
