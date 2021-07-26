d5649ee45909450e40b1b7bdb2ca3d4e7cf5d6c9
Ramiro Olivencia <ramiro@olivencia.com.ar>
2021-07-26 20:40:12 -0300

    Added solution to problem 1142

---
+++ 1-beginner/1142 - PUM.js
@@ -0,0 +1,16 @@
+// 1142 - Logical Sequence
+// URL - https://www.urionlinejudge.com.br/judge/en/problems/view/1142
+
+// const input = "7";
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+const value = parseInt(input);
+
+const multiplicator = (value) => 3 * (value - 1);
+
+for (let counter = 1; counter <= value; counter++) {
+  console.log(
+    `${counter + multiplicator(counter)} ${
+      counter + multiplicator(counter) + 1
+    } ${counter + multiplicator(counter) + 2} PUM`
+  );
+}
